40bdac1dbdfce5fc8f7bec1eff25ae58d2c9619a
inspectorgeneral36@gmail.com <inspectorgeneral36@gmail.com>
2017-09-17 12:31:24 +0100

    some changes made.....

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const path = require('path');
+const http=require("http");
 const express = require('express');
+const socketIo =require("socket.io");
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
+var server =http.createServer(app);
 app.use(express.static(publicPath));
+let io=socketIo(server);
 
-app.listen(port, () => {
+io.on('connection',(socket)=>{
+console.log("new User connected");
+});
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
