8313855c446e093bcb0ec9d5e62208c2460d120d
inspectorgeneral36@gmail.com <inspectorgeneral36@gmail.com>
2017-09-17 14:30:14 +0100

    Greet new users and existing users

--- server/server.js
+++ server/server.js
@@ -13,15 +13,34 @@ let io=socketIo(server);
 
 io.on('connection',(socket)=>{
 console.log("new User connected");
+socket.emit("newMessage",{
+from:"Admin",
+text:"Welcome to the chat app",
+createdAt:new Date().getTime()
+});
+
+socket.broadcast.emit("newMessage",{
+from:"Admin",
+text:"New user joined",
+createdAt:new Date().getTime()
+});
 
 
 socket.on("createMessage",(message)=>{
   console.log(message);
-  io.emit("newMessage",{
+  /**io.emit("newMessage",{
     from:message.from,
     text:message.text,
     createdAt:new Date().getTime()
-  });
+  });**/
+socket.broadcast.emit("newMessage",{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+});
+
+
+
 });
 
 
