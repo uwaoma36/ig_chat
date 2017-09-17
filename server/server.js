660cf7c31b1e92bae42f42befa9278d4600adf54
inspectorgeneral36@gmail.com <inspectorgeneral36@gmail.com>
2017-09-17 15:55:54 +0100

    created generateMessage utiliy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http=require("http");
 const express = require('express');
 const socketIo =require("socket.io");
-
+const {generateMessage}=require("./utils/message");
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -13,17 +13,10 @@ let io=socketIo(server);
 
 io.on('connection',(socket)=>{
 console.log("new User connected");
-socket.emit("newMessage",{
-from:"Admin",
-text:"Welcome to the chat app",
-createdAt:new Date().getTime()
-});
 
-socket.broadcast.emit("newMessage",{
-from:"Admin",
-text:"New user joined",
-createdAt:new Date().getTime()
-});
+socket.emit("newMessage",generateMessage('Admin','Welcome to the chat app'));
+
+socket.broadcast.emit("newMessage",generateMessage('Admin','New user joined'));
 
 
 socket.on("createMessage",(message)=>{
@@ -33,11 +26,7 @@ socket.on("createMessage",(message)=>{
     text:message.text,
     createdAt:new Date().getTime()
   });**/
-socket.broadcast.emit("newMessage",{
-    from:message.from,
-    text:message.text,
-    createdAt:new Date().getTime()
-});
+socket.broadcast.emit("newMessage",generateMessage(message.from,message.text));
 
 
 
---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+let expect=require("expect");
+
+let {generateMessage}=require("./message");
+
+describe('generateMessage',()=>{
+it('should generate correct message object',()=>{
+
+let from='jen';
+let text='some message';
+let message = generateMessage(from,text);
+
+
+expect(message.createdAt).toBeA('number');
+expect(message).toInclude({from,text});
+
+});
+
+});
